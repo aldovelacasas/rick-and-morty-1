9dbada1d23de73b4260e16ee5a08c894122bb1e1
aldovelacasas <aldo251_@hotmail.com>
2023-06-27 18:31:41 -0800

    commit2

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card(props) {
+   const {name, status, species, gender, origin, image} = props;
+   return (
+      <div>
+         <button onClick={()=> onclose()}>X</button>
+         <h2>{name}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin}</h2>
+         <img src={image} alt='' />
+      </div>
+   );
+}
