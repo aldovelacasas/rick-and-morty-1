ae513f58c5f304520ea492e59814243dc6cb8160
aldovelacasas <aldo251_@hotmail.com>
2023-07-10 14:49:27 -0800

    modificacion

--- src/components/About components/Aboutcomponentes.jsx
+++ src/components/About components/Aboutcomponentes.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return(
         <div>
-          <h1>SOY ALDO Y NO TUVE QUE FIRMAR HENRY</h1>
+          <h1>SOY ALDO </h1>
 
         </div>
     )
